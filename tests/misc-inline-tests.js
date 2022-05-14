54a4258732111a163ed64de93cbb2703bb46a56a
Eric Gade <eric.gade@gmail.com>
2022-05-14 13:50:28 -0400

    Updating grammar and misc inline tests based on real world examples

--- tests/misc-inline-tests.js
+++ tests/misc-inline-tests.js
@@ -20,4 +20,43 @@ describe("Miscellaneous Inline Tests", () => {
         let match = inlineGrammar.match("[ur-{d}ba-ba6]", "missingGrapheme");
         assert.isTrue(match.succeeded());
     });
+    it("Should parse basic sign with allograph", () => {
+        let match = inlineGrammar.match("PA~a", "grapheme");
+        assert.isTrue(match.succeeded());
+    });
+    it("Should be able to parse 4(|ASZxDISZ@t|)", () => {
+        let match = inlineGrammar.match("4(|ASZxDISZ@t|)", "grapheme");
+        assert.isTrue(match.succeeded());
+    });
+    it("Should be able to parse sila3-ta!(DUG)", () => {
+        let match = inlineGrammar.match("sila3-ta!(DUG)", "grapheme");
+        assert.isTrue(match.succeeded());
+    });
+    describe("Missing Word/Grapheme Cases", () => {
+        it.skip("Should be able to parse [dumu lu2-{d}szara2]", () => {
+            let match = inlineGrammar.match("[dumu lu2-{d}szara2]", "missingGrapheme");
+            assert.isTrue(match.succeeded());
+        });
+        // Other examples
+        // "[x a]-ha-ti"
+        // "[kiszib3 lugal]-e2-mah-e"
+        // "[iti x x {d}nin]-a-zu"
+    });
+    describe("Field Token Cases", () => {
+        it("Should be able to parse [...] = %a MIN<(mu-'-ir-ri)>", () => {
+            let match = inlineGrammar.match("[...] = %a MIN<(mu-'-ir-ri)>", "Words");
+            assert.isTrue(match.succeeded());
+        });
+    });
+
+    describe.skip("CDLI logograms", () => {
+        it("Should parse _a mu-ru_ as grapheme", () => {
+            let match = inlineGrammar.match("_a mu-ru_", "grapheme");
+            assert.isTrue(match.succeeded());
+        });
+        it("Should parse _a mu-ru_ as logogramCDLI", () => {
+            let match = inlineGrammar.match("_a mu-ru_", "logogramCDLI");
+            assert.isTrue(match.succeeded());
+        });
+    });
 });
